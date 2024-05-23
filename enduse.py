e530aecb074ee6f76f4170675ed89475a37095d9
Leranbychetan <learnbychetan@gmail.com>
2024-05-23 10:10:29 +0530

    23/05

---
+++ enduse.py
@@ -0,0 +1,9 @@
+# use of end keyword
+'''
+print("this is a boy",end=" ")
+print("this is ball",end=" ")
+print("this is apple",end=" ")
+'''
+print(12,45,67,89,sep="*",end="#")
+print(12,45,67,89,sep="@")
+print(12,45,67,89,sep="*",end="hi")
---
+++ printformate.py
@@ -0,0 +1,23 @@
+#addition program
+a=12
+b=5
+c=a+b
+'''
+#multiple object
+print("value of a = ",a)
+print("value of b = ",b)
+print("sum of",a,"and",b,"=",c)
+'''
+'''
+#format method
+print("sum of {} and {} = {}".format(a,b,c))
+print("sum of {0} and {1} = {2}".format(a,b,c))
+print("sum of {1} and {1} = {1}".format(a,b,c))
+'''
+#fstring
+print(f"sum of {a} and {b} = {c}")
+print(F"sum of {a} and {b} = {c}")
+
+
+
+
